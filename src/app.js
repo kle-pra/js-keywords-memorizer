e0cde76b7656365986b27e2c037744a97d38cc8c
klepra <klemen.pravdic@gmail.com>
2018-08-16 22:28:02 +0200

    simple logic with voice sounds

--- src/app.js
+++ src/app.js
@@ -3,11 +3,27 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import "./css/style.css";
 
-const keywords = ['let', 'const', 'var', 'for', 'if', 'typeof'];
+const keywords = ['abstract', 'arguments', 'await', 'boolean',
+  'break', 'byte', 'case', , 'catch',
+  'char', 'class', 'const', 'continue',
+  'debugger', 'default', 'delete', 'do',
+  'double', 'else', 'enum', 'eval',
+  'export', 'extends', 'false', 'final',
+  'finally', 'float', 'for', 'function',
+  'goto', 'if', 'implements', 'import',
+  'instanceof', 'int', 'interface',
+  'let', 'long', 'native', 'new',
+  'null', 'package', 'private', 'protected',
+  'public', 'return', 'short', 'static',
+  'super', 'switch', 'synchronized', 'this',
+  'throw', 'throws', 'transient', 'true',
+  'try', 'typeof', 'var', 'void',
+  'volatile', 'while', 'with', 'yield'];
+
 let timeLeft = 5;
 let currentScore = 0;
 let gameOver = false;
-
+let gameIsOn = false;
 const score = document.getElementById('score');
 const time = document.getElementById('time');
 const input = document.getElementById('input');
@@ -18,40 +34,63 @@ window.addEventListener('load', initGame);
 input.addEventListener('keyup', matchWord);
 
 function initGame() {
-  // load random keyword
-  word.innerHTML = keywords[Math.floor(Math.random() * keywords.length)];
-  //countdown
+  setupRandomWord()
+
   setInterval(() => {
-    if (timeLeft > 0) {
-      timeLeft -= 1;
-      time.innerHTML = timeLeft;
-    } else {
-      gameOver = true;
+    if (gameIsOn) {
+      if (!gameOver && timeLeft > 0) {
+        timeLeft -= 1;
+        time.innerHTML = timeLeft;
+      } else {
+        gameOver = true;
+      }
     }
   }, 1000);
 
   setInterval(() => {
-    if (gameOver) {
-      alert("Game over! Score is " + currentScore);
-      currentScore = 0;
-      score.innerHTML = currentScore;
-      timeLeft = 5;
-      gameOver = false;
+    if (gameIsOn) {
+      if (gameOver) {
+
+        const keyword = 'Game over! Your score is ' + currentScore;
+        var msg = new SpeechSynthesisUtterance(keyword);
+        window.speechSynthesis.speak(msg);
+
+        alert("Game over! Score is " + currentScore);
+        currentScore = 0;
+        score.innerHTML = currentScore;
+        gameOver = false;
+        gameIsOn = false;
+        timeLeft = 5;
+      }
     }
   }, 100);
 }
 
 function matchWord(e) {
+  gameIsOn = true;
   const val = input.value;
-  console.log(e);
+
   if (val === word.innerHTML) {
-    currentScore += 50;
+    if (!gameOver) {
+      currentScore += 50;
+      const keyword = 'Correct!';
+      var msg = new SpeechSynthesisUtterance(keyword);
+      window.speechSynthesis.speak(msg);
+    }
     score.innerHTML = currentScore;
     gameOver = false;
     timeLeft = 5;
     input.value = "";
-    // load random keyword
-    word.innerHTML = keywords[Math.floor(Math.random() * keywords.length)];
+    setupRandomWord();
   }
+}
+
+
+function setupRandomWord() {
+  const keyword = keywords[Math.floor(Math.random() * keywords.length)];
+  var msg = new SpeechSynthesisUtterance(keyword);
+  window.speechSynthesis.speak(msg);
+  word.innerHTML = keyword;
+
 }
 
